41401275ec06669767fd603a3bd91ac93f2200cc
r0nsha <r0nsh4v1t@gmail.com>
2023-07-27 15:26:13 +0300

    wip

--- src/state.rs
+++ src/state.rs
@@ -1,7 +1,13 @@
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
+
 use crate::span::SourceCache;
 
 #[derive(Debug)]
 pub struct State {
+    root_dir: PathBuf,
     pub options: CompilerOptions,
     pub source_cache: SourceCache,
 }
@@ -9,9 +15,14 @@ pub struct State {
 impl State {
     pub fn new(options: CompilerOptions) -> Self {
         Self {
+            root_dir: env::current_dir().unwrap(),
             options,
             source_cache: SourceCache::new(),
         }
+    }
+
+    pub fn root_dir(&self) -> &Path {
+        &self.root_dir
     }
 }
 
