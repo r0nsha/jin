f0e1555637d0f3bd1c218033106514aea4b913bc
r0nsha <r0nsh4v1t@gmail.com>
2023-08-05 19:12:17 +0300

    hir pretty: type annotations

--- src/hir/pretty_print.rs
+++ src/hir/pretty_print.rs
@@ -1,8 +1,9 @@
-use std::io;
-
 use pretty::{Doc, RcDoc};
 
-use crate::db::{Database, TypeId};
+use crate::{
+    db::{Database, TypeId},
+    ty::Type,
+};
 
 use super::*;
 
@@ -16,7 +17,7 @@ pub(crate) fn print_module(db: &Database, module: &Module) {
     .append(RcDoc::line())
     .append(RcDoc::intersperse(
         module.bindings.iter().map(|b| b.to_doc(db)),
-        Doc::line(),
+        RcDoc::line(),
     ))
     .nest(1)
     .append(RcDoc::line())
@@ -25,12 +26,12 @@ pub(crate) fn print_module(db: &Database, module: &Module) {
     println!("{}", doc.pretty(80));
 }
 
-trait ToDoc<'a> {
-    fn to_doc(&self, db: &'a Database) -> RcDoc<()>;
+trait ToDoc<'a, 'd> {
+    fn to_doc(&self, db: &'a Database) -> RcDoc<'d, ()>;
 }
 
-impl ToDoc<'_> for Hir {
-    fn to_doc(&self, db: &'_ Database) -> RcDoc<()> {
+impl<'a, 'd> ToDoc<'a, 'd> for Hir {
+    fn to_doc(&self, db: &'a Database) -> RcDoc<'d, ()> {
         match self {
             Hir::Fun(x) => x.to_doc(db),
             Hir::Block(x) => x.to_doc(db),
@@ -40,53 +41,71 @@ impl ToDoc<'_> for Hir {
     }
 }
 
-impl ToDoc<'_> for Binding {
-    fn to_doc(&self, db: &'_ Database) -> RcDoc<()> {
+impl<'a, 'd> ToDoc<'a, 'd> for Binding {
+    fn to_doc(&self, db: &'a Database) -> RcDoc<'d, ()> {
         RcDoc::text("let")
-            .append(Doc::space())
-            .append(Doc::text(self.name.as_str()))
-            .append(Doc::space())
-            .append(Doc::text("="))
-            .append(Doc::space())
+            .append(RcDoc::space())
+            .append(RcDoc::text(self.name.as_str()))
+            .append(RcDoc::space())
+            .append(self.id.get(db).ty.to_doc(db))
+            .append(RcDoc::softline())
+            .append(RcDoc::text("="))
+            .append(RcDoc::space())
             .append(self.expr.to_doc(db))
     }
 }
 
-impl ToDoc<'_> for Fun {
-    fn to_doc(&self, db: &'_ Database) -> RcDoc<()> {
+impl<'a, 'd> ToDoc<'a, 'd> for Fun {
+    fn to_doc(&self, db: &'a Database) -> RcDoc<'d, ()> {
+        let ret_ty = self.ty.get(db).kind.as_fun().unwrap().ret.to_doc(db);
+
         RcDoc::text("fn()")
-            .append(Doc::space())
+            .append(RcDoc::space())
+            .append(ret_ty)
+            .append(RcDoc::space())
             .append(self.body.to_doc(db))
     }
 }
 
-impl ToDoc<'_> for Block {
-    fn to_doc(&self, db: &'_ Database) -> RcDoc<()> {
+impl<'a, 'd> ToDoc<'a, 'd> for Block {
+    fn to_doc(&self, db: &'a Database) -> RcDoc<'d, ()> {
         RcDoc::text("{")
-            .append(RcDoc::softline())
+            .append(RcDoc::line())
             .append(RcDoc::intersperse(
                 self.exprs.iter().map(|e| e.to_doc(db)),
-                Doc::line(),
+                RcDoc::line(),
             ))
             .nest(1)
-            .append(RcDoc::softline())
+            .append(RcDoc::line())
             .append("}")
     }
 }
 
-impl ToDoc<'_> for Ret {
-    fn to_doc(&self, db: &'_ Database) -> RcDoc<()> {
+impl<'a, 'd> ToDoc<'a, 'd> for Ret {
+    fn to_doc(&self, db: &'a Database) -> RcDoc<'d, ()> {
         RcDoc::text("return")
-            .append(Doc::space())
+            .append(RcDoc::space())
             .append(self.expr.as_ref().map_or(RcDoc::nil(), |e| e.to_doc(db)))
     }
 }
 
-impl ToDoc<'_> for Lit {
-    fn to_doc(&self, _db: &'_ Database) -> RcDoc<()> {
+impl<'a, 'd> ToDoc<'a, 'd> for Lit {
+    fn to_doc(&self, _db: &'a Database) -> RcDoc<'d, ()> {
         match &self.kind {
             LitKind::Int(v) => RcDoc::text(v.to_string()),
             LitKind::Unit => RcDoc::text("()"),
         }
+    }
+}
+
+impl<'a, 'd> ToDoc<'a, 'd> for TypeId {
+    fn to_doc(&self, db: &'a Database) -> RcDoc<'d, ()> {
+        self.get(db).to_doc(db)
+    }
+}
+
+impl<'a, 'd> ToDoc<'a, 'd> for Type {
+    fn to_doc(&self, _db: &'a Database) -> RcDoc<'d, ()> {
+        RcDoc::text(self.to_string())
     }
 }
