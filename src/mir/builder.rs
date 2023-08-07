47526a4dd464d8bc3201821c2d67d22b7ab05d84
r0nsha <r0nsh4v1t@gmail.com>
2023-08-07 17:06:24 +0300

    wip

--- src/mir/builder.rs
+++ src/mir/builder.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::db::FunctionId;
 
 use super::*;
@@ -11,6 +13,38 @@ impl FunctionBuilder {
         Self {
             f: Function::new(id),
         }
+    }
+
+    #[inline]
+    pub(crate) fn block(&self, id: BlockId) -> &Block {
+        &self.f.cfg.blocks[id]
+    }
+
+    #[inline]
+    pub(crate) fn block_mut(&mut self, id: BlockId) -> &mut Block {
+        &mut self.f.cfg.blocks[id]
+    }
+
+    #[inline]
+    pub(crate) fn start_block(&self) -> &Block {
+        self.block(BlockId(0))
+    }
+
+    #[inline]
+    pub(crate) fn start_block_mut(&mut self) -> &mut Block {
+        self.block_mut(BlockId(0))
+    }
+
+    pub(crate) fn reachable_blocks(&self) -> HashSet<BlockId> {
+        let mut reachable = HashSet::new();
+
+        for blk in self.f.cfg.blocks.iter() {
+            if !blk.predecessors.is_empty() {
+                reachable.insert(blk.id);
+            }
+        }
+
+        reachable
     }
 
     pub(crate) fn create_register(&mut self, reg: Register) -> RegisterId {
@@ -28,12 +62,9 @@ impl FunctionBuilder {
         self.f.cfg.blocks.as_slice().last().unwrap()
     }
 
-    pub(crate) fn block(&self, id: BlockId) -> Option<&Block> {
-        self.f.cfg.blocks.get(id)
-    }
-
-    pub(crate) fn block_mut(&mut self, id: BlockId) -> Option<&mut Block> {
-        self.f.cfg.blocks.get_mut(id)
+    pub(crate) fn create_edge(&mut self, source: BlockId, target: BlockId) {
+        self.f.cfg.blocks[target].predecessors.push(source);
+        self.f.cfg.blocks[source].successors.push(target);
     }
 
     pub(crate) fn finish(self) -> Result<Function, String> {
