6d6bfd89de1012e78d849c20c91dd0ec9aead0bf
r0nsha <r0nsh4v1t@gmail.com>
2023-08-07 21:32:57 +0300

    add type annotations to register allocations

--- src/mir/pretty_print.rs
+++ src/mir/pretty_print.rs
@@ -6,56 +6,53 @@ use super::*;
 
 pub(crate) fn print(db: &Database, mir: &Mir) {
     let doc = RcDoc::intersperse(
-        mir.functions.iter().map(|f| f.to_doc(db)),
+        mir.functions.iter().map(|f| print_function(db, f)),
         RcDoc::hardline().append(RcDoc::hardline()),
     );
 
     println!("{}", doc.pretty(80));
 }
 
-trait ToDoc<'db, 'd> {
-    fn to_doc(&self, db: &'db Database) -> RcDoc<'d, ()>;
+fn print_function<'db, 'd>(db: &'db Database, fun: &Function) -> RcDoc<'d, ()> {
+    let ret_ty = fun
+        .id
+        .get(db)
+        .ty
+        .get(db)
+        .kind
+        .as_function()
+        .unwrap()
+        .ret
+        .to_doc(db, fun);
+
+    RcDoc::text("fn")
+        .append(RcDoc::space())
+        .append(RcDoc::text(fun.id().get(db).name.standard_full_name()))
+        .append(RcDoc::text("()"))
+        .append(RcDoc::space())
+        .append(ret_ty)
+        .append(RcDoc::space())
+        .append(RcDoc::text("{"))
+        .append(RcDoc::hardline())
+        .append(RcDoc::intersperse(
+            fun.blocks().iter().map(|blk| blk.to_doc(db, fun)),
+            RcDoc::hardline(),
+        ))
+        .append(RcDoc::hardline())
+        .append("}")
 }
 
-impl<'db, 'd> ToDoc<'db, 'd> for Function {
-    fn to_doc(&self, db: &'db Database) -> RcDoc<'d, ()> {
-        let ret_ty = self
-            .id
-            .get(db)
-            .ty
-            .get(db)
-            .kind
-            .as_function()
-            .unwrap()
-            .ret
-            .to_doc(db);
-
-        RcDoc::text("fn")
-            .append(RcDoc::space())
-            .append(RcDoc::text(self.id().get(db).name.standard_full_name()))
-            .append(RcDoc::text("()"))
-            .append(RcDoc::space())
-            .append(ret_ty)
-            .append(RcDoc::space())
-            .append(RcDoc::text("{"))
-            .append(RcDoc::hardline())
-            .append(RcDoc::intersperse(
-                self.blocks().iter().map(|blk| blk.to_doc(db)),
-                RcDoc::hardline(),
-            ))
-            // .nest(1)
-            .append(RcDoc::hardline())
-            .append("}")
-    }
+trait ToDoc<'db, 'd> {
+    fn to_doc(&self, db: &'db Database, fun: &'db Function) -> RcDoc<'d, ()>;
 }
 
 impl<'db, 'd> ToDoc<'db, 'd> for Block {
-    fn to_doc(&self, db: &'db Database) -> RcDoc<'d, ()> {
+    fn to_doc(&self, db: &'db Database, fun: &'db Function) -> RcDoc<'d, ()> {
         RcDoc::text("@")
             .append(RcDoc::text(self.name.as_str()))
             .append(RcDoc::hardline())
             .append(RcDoc::intersperse(
-                self.instructions.iter().map(|inst| inst.to_doc(db)),
+                self.instructions.iter().map(|inst| inst.to_doc(db, fun)),
                 RcDoc::hardline(),
             ))
             .nest(1)
@@ -63,39 +60,47 @@ impl<'db, 'd> ToDoc<'db, 'd> for Block {
 }
 
 impl<'db, 'd> ToDoc<'db, 'd> for Instruction {
-    fn to_doc(&self, db: &'db Database) -> RcDoc<'d, ()> {
+    fn to_doc(&self, db: &'db Database, fun: &'db Function) -> RcDoc<'d, ()> {
         match self {
             Instruction::Return(ret) => RcDoc::text("ret")
                 .append(RcDoc::space())
-                .append(ret.register.to_doc(db)),
+                .append(ret.register.to_doc(db, fun)),
             Instruction::IntLit(lit) => {
-                register_start(db, lit.register).append(RcDoc::text(lit.value.to_string()))
+                register_start(db, fun, lit.register).append(RcDoc::text(lit.value.to_string()))
             }
-            Instruction::UnitLit(lit) => register_start(db, lit.register).append(RcDoc::text("()")),
+            Instruction::UnitLit(lit) => {
+                register_start(db, fun, lit.register).append(RcDoc::text("()"))
+            }
         }
     }
 }
 
 impl<'db, 'd> ToDoc<'db, 'd> for RegisterId {
-    fn to_doc(&self, db: &'db Database) -> RcDoc<'d, ()> {
+    fn to_doc(&self, _db: &'db Database, _fun: &'db Function) -> RcDoc<'d, ()> {
         RcDoc::text("%").append(RcDoc::text(self.0.to_string()))
     }
 }
 
 impl<'db, 'd> ToDoc<'db, 'd> for TyId {
-    fn to_doc(&self, db: &'db Database) -> RcDoc<'d, ()> {
-        self.get(db).to_doc(db)
+    fn to_doc(&self, db: &'db Database, fun: &'db Function) -> RcDoc<'d, ()> {
+        self.get(db).to_doc(db, fun)
     }
 }
 
 impl<'db, 'd> ToDoc<'db, 'd> for Ty {
-    fn to_doc(&self, _db: &'db Database) -> RcDoc<'d, ()> {
+    fn to_doc(&self, _db: &'db Database, _fun: &'db Function) -> RcDoc<'d, ()> {
         RcDoc::text(self.to_string())
     }
 }
 
-fn register_start<'db, 'd>(db: &'db Database, reg: RegisterId) -> RcDoc<'d, ()> {
-    reg.to_doc(db)
+fn register_start<'db, 'd>(
+    db: &'db Database,
+    fun: &'db Function,
+    reg: RegisterId,
+) -> RcDoc<'d, ()> {
+    reg.to_doc(db, fun)
+        .append(RcDoc::space())
+        .append(fun.register(reg).unwrap().ty.to_doc(db, fun))
         .append(RcDoc::space())
         .append(RcDoc::text("="))
         .append(RcDoc::space())
