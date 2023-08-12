32c5f3266eebf9a460ac812c9bf43fb33e6235b0
r0nsha <r0nsh4v1t@gmail.com>
2023-08-12 14:20:41 +0300

    wip

--- src/common.rs
+++ src/common.rs
@@ -1,9 +1,6 @@
 pub(crate) mod index_vec;
 pub(crate) mod qualified_name;
-pub(crate) mod scopes;
 pub(crate) mod time;
 
 pub(crate) use index_vec::*;
 pub(crate) use qualified_name::*;
-#[allow(unused)]
-pub(crate) use scopes::*;
