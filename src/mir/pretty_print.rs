d69224d9d6be6f8e83e9b0e3c0542e8b1a83ee6b
r0nsha <r0nsh4v1t@gmail.com>
2023-08-18 22:29:46 +0300

    pretty print

--- src/mir/pretty_print.rs
+++ src/mir/pretty_print.rs
@@ -38,8 +38,7 @@ trait ToDoc<'db, 'd> {
 
 impl<'db, 'd> ToDoc<'db, 'd> for Block {
     fn to_doc(&self, db: &'db Database, fun: &'db Function) -> RcDoc<'d, ()> {
-        RcDoc::text("@")
-            .append(RcDoc::text(self.name.as_str()))
+        block_name(self)
             .append(RcDoc::hardline())
             .append(RcDoc::intersperse(
                 self.instructions.iter().map(|inst| inst.to_doc(db, fun)),
@@ -55,9 +54,28 @@ impl<'db, 'd> ToDoc<'db, 'd> for Instruction {
             Self::Return(ret) => {
                 RcDoc::text("ret").append(RcDoc::space()).append(ret.value.to_doc(db, fun))
             }
-            Self::Jmp(jmp) => {}
-            Self::Jnz(jnz) => {}
-            Self::Phi(phi) => {}
+            Self::Jmp(jmp) => RcDoc::text("jmp")
+                .append(RcDoc::space())
+                .append(block_name(fun.block(jmp.target).unwrap())),
+            Self::Jnz(jnz) => RcDoc::text("jnz")
+                .append(RcDoc::space())
+                .append(jnz.cond.to_doc(db, fun))
+                .append(RcDoc::space())
+                .append(block_name(fun.block(jnz.b1).unwrap()))
+                .append(RcDoc::space())
+                .append(block_name(fun.block(jnz.b2).unwrap())),
+            Self::Phi(phi) => RcDoc::text("phi").append(RcDoc::space()).append(RcDoc::intersperse(
+                phi.values.iter().map(|(blk, value)| {
+                    RcDoc::text("(")
+                        .append(block_name(fun.block(*blk).unwrap()))
+                        .append(RcDoc::space())
+                        .append(RcDoc::text("->"))
+                        .append(RcDoc::space())
+                        .append(value.to_doc(db, fun))
+                        .append(RcDoc::text(")"))
+                }),
+                RcDoc::space(),
+            )),
             Self::Call(call) => register_alloc(db, fun, call.register)
                 .append(RcDoc::text("call"))
                 .append(RcDoc::space())
@@ -123,4 +141,8 @@ fn binary_instruction_name(op: BinaryOp) -> String {
     let prefix = "i";
     let inst = op.name();
     format!("{prefix}{inst}")
+}
+
+fn block_name<'d>(blk: &Block) -> RcDoc<'d, ()> {
+    RcDoc::text(format!("@{}", blk.name))
 }
