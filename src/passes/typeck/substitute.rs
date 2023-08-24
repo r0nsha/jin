1114291c4a3ae14b357cbd6d88fec3ac2d9faf51
r0nsha <r0nsh4v1t@gmail.com>
2023-08-25 01:24:09 +0300

    wip

--- src/passes/typeck/substitute.rs
+++ src/passes/typeck/substitute.rs
@@ -15,7 +15,7 @@ impl<'db> InferCtxt<'db> {
 
         for i in 0..self.db.symbols.len() {
             let ty = self.db.symbols[i.into()].ty;
-            self.db.symbols[i.into()].ty = self.substitute_ty(ty, &mut unbound_vars);
+            self.db.symbols[i.into()].ty = substitute_ty(self, ty, &mut unbound_vars);
         }
 
         for item in &mut tast.items {
@@ -24,47 +24,47 @@ impl<'db> InferCtxt<'db> {
 
         unbound_vars
     }
+}
 
-    fn substitute_ty(&mut self, ty: Type, unbound_vars: &mut HashSet<TypeVar>) -> Type {
-        Type::new(self.substitute_tykind(&ty, unbound_vars))
-    }
+fn substitute_ty(cx: &mut InferCtxt, ty: Type, unbound_vars: &mut HashSet<TypeVar>) -> Type {
+    Type::new(substitute_tykind(cx, &ty, unbound_vars))
+}
 
-    fn substitute_tykind(
-        &mut self,
-        ty: &TypeKind,
-        unbound_vars: &mut HashSet<TypeVar>,
-    ) -> TypeKind {
-        match ty {
-            TypeKind::Function(fun) => TypeKind::Function(FunctionType {
-                ret: self.substitute_ty(fun.ret, unbound_vars),
-                params: fun
-                    .params
-                    .iter()
-                    .map(|param| FunctionTypeParam {
-                        name: param.name,
-                        ty: self.substitute_ty(param.ty, unbound_vars),
-                    })
-                    .collect(),
-            }),
-            TypeKind::Infer(InferType::TypeVar(var)) => {
-                let root = self.ty_unification_table.find(*var);
+fn substitute_tykind(
+    cx: &mut InferCtxt,
+    ty: &TypeKind,
+    unbound_vars: &mut HashSet<TypeVar>,
+) -> TypeKind {
+    match ty {
+        TypeKind::Function(fun) => TypeKind::Function(FunctionType {
+            ret: substitute_ty(cx, fun.ret, unbound_vars),
+            params: fun
+                .params
+                .iter()
+                .map(|param| FunctionTypeParam {
+                    name: param.name,
+                    ty: substitute_ty(cx, param.ty, unbound_vars),
+                })
+                .collect(),
+        }),
+        TypeKind::Infer(InferType::TypeVar(var)) => {
+            let root = cx.ty_unification_table.find(*var);
 
-                if let Some(ty) = self.ty_unification_table.probe_value(root) {
-                    self.substitute_tykind(&ty, unbound_vars)
-                } else {
-                    unbound_vars.insert(root);
-                    TypeKind::Infer(InferType::TypeVar(root))
-                }
+            if let Some(ty) = cx.ty_unification_table.probe_value(root) {
+                substitute_tykind(cx, &ty, unbound_vars)
+            } else {
+                unbound_vars.insert(root);
+                TypeKind::Infer(InferType::TypeVar(root))
             }
-            TypeKind::Infer(InferType::IntVar(var)) => {
-                let root = self.int_unification_table.find(*var);
-
-                self.int_unification_table
-                    .probe_value(root)
-                    .map_or_else(|| TypeKind::DEFAULT_INT, Into::into)
-            }
-            _ => ty.clone(),
         }
+        TypeKind::Infer(InferType::IntVar(var)) => {
+            let root = cx.int_unification_table.find(*var);
+
+            cx.int_unification_table
+                .probe_value(root)
+                .map_or_else(|| TypeKind::DEFAULT_INT, Into::into)
+        }
+        _ => ty.clone(),
     }
 }
 
@@ -84,7 +84,7 @@ impl Substitute<'_> for Expr {
             Self::Name(_) | Self::Lit(_) => (),
         }
 
-        self.set_ty(cx.substitute_ty(self.ty(), unbound_vars));
+        self.set_ty(substitute_ty(cx, self.ty(), unbound_vars));
     }
 }
 
@@ -94,7 +94,7 @@ impl Substitute<'_> for Item {
             ItemKind::Function(fun) => fun.substitute(cx, unbound_vars),
         }
 
-        self.ty = cx.substitute_ty(self.ty, unbound_vars);
+        self.ty = substitute_ty(cx, self.ty, unbound_vars);
     }
 }
 
@@ -102,14 +102,14 @@ impl Substitute<'_> for Function {
     fn substitute(&mut self, cx: &mut InferCtxt<'_>, unbound_vars: &mut HashSet<TypeVar>) {
         self.sig.substitute(cx, unbound_vars);
         self.body.substitute(cx, unbound_vars);
-        self.ty = cx.substitute_ty(self.ty, unbound_vars);
+        self.ty = substitute_ty(cx, self.ty, unbound_vars);
     }
 }
 
 impl Substitute<'_> for FunctionSig {
     fn substitute(&mut self, cx: &mut InferCtxt<'_>, unbound_vars: &mut HashSet<TypeVar>) {
         for param in &mut self.params {
-            param.ty = cx.substitute_ty(param.ty, unbound_vars);
+            param.ty = substitute_ty(cx, param.ty, unbound_vars);
         }
     }
 }
@@ -147,7 +147,7 @@ impl Substitute<'_> for Call {
                 }
             }
 
-            arg.set_ty(cx.substitute_ty(arg.ty(), unbound_vars));
+            arg.set_ty(substitute_ty(cx, arg.ty(), unbound_vars));
         }
     }
 }
