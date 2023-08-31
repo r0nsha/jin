897e26818e150323e3f7f3c79089c6adeec3c7b1
r0nsha <r0nsh4v1t@gmail.com>
2023-08-31 19:15:41 +0300

    wip

--- src/passes/monomorphization.rs
+++ src/passes/monomorphization.rs
@@ -2,7 +2,9 @@ use crate::{db::Db, hir::Hir};
 
 pub fn monomorphization(db: &mut Db, hir: &mut Hir) {
     // TODO: collect mono items
-    // TODO: find polymorphic function uses in mono items
-    // TODO: generate the appropriate "hir",
+    // TODO: find polymorphic function uses in mono items, recursively, marking visited items to
+    //       not visit them twice
+    // TODO: generate the appropriate definition for each use
+    // TODO: replace the used places with the new definition
 }
 
