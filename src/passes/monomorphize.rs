2f52f703a5a132781a01f6981e395791370dc6bb
r0nsha <r0nsh4v1t@gmail.com>
2023-09-02 17:06:17 +0300

    wip

--- src/passes/monomorphize.rs
+++ src/passes/monomorphize.rs
@@ -1,32 +1,31 @@
-use ustr::ustr;
+use std::collections::HashSet;
 
 use crate::{
-    db::{Db, Def, DefId},
+    db::{Db, DefId},
     hir::{Fn, Hir, Item, ItemKind},
     passes::subst::SubstTy,
     span::Span,
     ty::{fold::TyFolder, Ty, TyKind},
 };
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub struct MonoItem {
+    pub id: DefId,
     pub args: Vec<Ty>,
-    pub source_id: DefId,
-    pub target_id: DefId,
 }
 
-pub fn monomorphize(db: &mut Db, hir: &Hir) -> Vec<MonoItem> {
+pub fn monomorphize(db: &mut Db, hir: &Hir) -> HashSet<MonoItem> {
     Collector::new(db).collect(hir).mono_items
 }
 
 struct Collector<'db> {
     db: &'db mut Db,
-    mono_items: Vec<MonoItem>,
+    mono_items: HashSet<MonoItem>,
 }
 
 impl<'db> Collector<'db> {
     fn new(db: &'db mut Db) -> Self {
-        Self { db, mono_items: vec![] }
+        Self { db, mono_items: HashSet::new() }
     }
 
     fn collect(mut self, hir: &Hir) -> Self {
@@ -49,41 +48,12 @@ impl<'db> Collector<'db> {
 
     fn collect_fn(&mut self, fun: &Fn) {
         for param in &fun.sig.params {
-            self.collect_mono_def(param.id, &[]);
+            self.collect_def_use(param.id, vec![]);
         }
     }
 
-    fn collect_mono_def(&mut self, source_id: DefId, args: &[Ty]) -> DefId {
-        if let Some(item) =
-            self.mono_items.iter().find(|item| item.source_id == source_id && item.args == args)
-        {
-            item.target_id
-        } else {
-            let target_id = self.alloc_mono_def(source_id, args);
-            self.mono_items.push(MonoItem { args: args.to_owned(), source_id, target_id });
-            target_id
-        }
-    }
-
-    fn alloc_mono_def(&mut self, id: DefId, args: &[Ty]) -> DefId {
-        let def = &self.db[id];
-
-        let new_qpath = if def.kind.is_fn() {
-            let args_str =
-                args.iter().map(|t| t.to_string(self.db)).collect::<Vec<String>>().join("_");
-            def.qpath.clone().with_name(ustr(&format!("{}${}", def.name, args_str)))
-        } else {
-            def.qpath.clone()
-        };
-
-        let new_scope = def.scope.clone();
-        let new_kind = def.kind.as_ref().clone();
-        let new_span = def.span;
-
-        let ty = def.ty;
-        let new_ty = ParamFolder { db: self.db, args }.fold(ty);
-
-        Def::alloc(self.db, new_qpath, new_scope, new_kind, new_ty, new_span)
+    fn collect_def_use(&mut self, source_id: DefId, args: Vec<Ty>) {
+        self.mono_items.insert(MonoItem { args, id: source_id });
     }
 }
 
