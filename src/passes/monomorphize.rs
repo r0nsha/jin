db424cfa0b2362d8d62d6a6b373e57210adabf32
r0nsha <r0nsh4v1t@gmail.com>
2023-09-02 17:41:17 +0300

    wip

--- src/passes/monomorphize.rs
+++ src/passes/monomorphize.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 use crate::{
     db::{Db, DefId},
-    hir::{Fn, Hir, Item, ItemKind},
+    hir::{Fn, Hir, HirVisitor, Item, ItemKind, Name},
     passes::subst::SubstTy,
     span::Span,
     ty::{fold::TyFolder, Ty, TyKind},
@@ -15,21 +15,22 @@ pub struct MonoItem {
 }
 
 pub fn monomorphize(db: &mut Db, hir: &Hir) -> HashSet<MonoItem> {
-    Collector::new(db).collect(hir).mono_items
+    Collector::new(db, hir).collect().mono_items
 }
 
 struct Collector<'db> {
     db: &'db mut Db,
+    hir: &'db Hir,
     mono_items: HashSet<MonoItem>,
 }
 
 impl<'db> Collector<'db> {
-    fn new(db: &'db mut Db) -> Self {
-        Self { db, mono_items: HashSet::new() }
+    fn new(db: &'db mut Db, hir: &Hir) -> Self {
+        Self { db, hir, mono_items: HashSet::new() }
     }
 
-    fn collect(mut self, hir: &Hir) -> Self {
-        for item in &hir.items {
+    fn collect(mut self) -> Self {
+        for item in &self.hir.items {
             self.collect_root_item(item);
         }
 
@@ -39,21 +40,51 @@ impl<'db> Collector<'db> {
     fn collect_root_item(&mut self, item: &Item) {
         match &item.kind {
             ItemKind::Fn(fun) => {
-                if fun.ty.is_polymorphic() {
-                    self.collect_fn(fun);
+                if !fun.ty.is_polymorphic() {
+                    self.collect_root_fn(fun);
                 }
             }
         }
     }
 
-    fn collect_fn(&mut self, fun: &Fn) {
+    fn collect_poly_item(&mut self, id: DefId) {
+        let item = self.hir.items.iter().find(|item| match &item.kind {
+            ItemKind::Fn(f) => f.id == id,
+        });
+
+        match &item.kind {
+            ItemKind::Fn(fun) => {
+                if !fun.ty.is_polymorphic() {
+                    self.collect_root_fn(fun);
+                }
+            }
+        }
+    }
+
+    fn collect_root_fn(&mut self, fun: &Fn) {
         for param in &fun.sig.params {
             self.collect_def_use(param.id, vec![]);
         }
     }
 
-    fn collect_def_use(&mut self, source_id: DefId, args: Vec<Ty>) {
-        self.mono_items.insert(MonoItem { args, id: source_id });
+    fn collect_def_use(&mut self, id: DefId, args: Vec<Ty>) {
+        self.mono_items.insert(MonoItem { args, id });
+    }
+}
+
+impl HirVisitor for Collector<'_> {
+    fn visit_fn(&mut self, f: &Fn) {
+        for p in &f.sig.params {
+            if p.ty.is_polymorphic() {
+                self.collect_def_use(p.id, vec![]);
+            }
+        }
+    }
+
+    fn visit_name(&mut self, name: &Name) {
+        if !name.args.is_empty() {
+            self.collect_def_use(name.id, name.args.clone());
+        }
     }
 }
 
