7c687d63e4a7331345a8f8dbcf7c98048804e37c
r0nsha <r0nsh4v1t@gmail.com>
2023-09-03 23:44:47 +0300

    adjustments

--- src/passes/typeck/apply_adjustments.rs
+++ src/passes/typeck/apply_adjustments.rs
@@ -1,5 +1,53 @@
-use crate::db::Db;
-use crate::hir::Hir;
-use crate::ty::tcx::TyCtxt;
+use crate::{
+    db::Db,
+    hir::{Expr, Fn, Hir, ItemKind},
+    ty::{adjust::Adjustments, tcx::TyCtxt},
+};
 
-pub fn apply_adjustments(db: &mut Db, tcx: &TyCtxt, hir: &mut Hir) {}
+pub fn apply_adjustments(db: &mut Db, tcx: &mut TyCtxt, hir: &mut Hir) {
+    let mut cx = Context { db, tcx };
+    for item in &mut hir.items {
+        match &mut item.kind {
+            ItemKind::Fn(f) => f.apply_adjustments(&mut cx),
+        }
+    }
+}
+
+struct Context<'db> {
+    db: &'db mut Db,
+    tcx: &'db mut TyCtxt,
+}
+
+trait Apply<'db> {
+    fn apply_adjustments(&mut self, cx: &mut Context<'db>);
+}
+
+impl Apply<'_> for Fn {
+    fn apply_adjustments(&mut self, cx: &mut Context<'_>) {
+        self.body.apply_adjustments(cx);
+    }
+}
+
+impl Apply<'_> for Expr {
+    fn apply_adjustments(&mut self, cx: &mut Context<'_>) {
+        match self {
+            Expr::If(if_) => todo!(),
+            Expr::Block(blk) => todo!(),
+            Expr::Return(ret) => todo!(),
+            Expr::Call(call) => todo!(),
+            Expr::Bin(bin) => todo!(),
+            Expr::Name(name) => todo!(),
+            Expr::Lit(_) => (),
+        }
+
+        apply_adjustments_to_expr(self, cx);
+    }
+}
+
+fn apply_adjustments_to_expr(expr: &mut Expr, cx: &Context<'_>) {
+    let adjustments = cx.tcx.adjustments[expr.id];
+
+    if !adjustments.is_empty() {
+        todo!("apply here...");
+    }
+}
