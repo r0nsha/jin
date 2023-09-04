a5f0f01a07acbe2103c847cc6cb4aa8ba9eb635a
r0nsha <r0nsh4v1t@gmail.com>
2023-09-04 19:36:07 +0300

    wip

--- src/ty/adjust.rs
+++ src/ty/adjust.rs
@@ -1,6 +1,42 @@
-pub type Adjustments = Vec<Adjustment>;
+use crate::{hir::Expr, ty::Ty};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub enum Adjustment {
-    NeverCoercion,
+pub struct Adjustments(Vec<Adjust>);
+
+impl Adjustments {
+    pub fn new() -> Self {
+        Self(vec![])
+    }
+
+    pub fn push(&mut self, adj: Adjust) {
+        self.0.push(adj);
+    }
+
+    pub fn apply(&self, mut expr: Expr) -> Expr {
+        for adj in &self.0 {
+            expr = match adj.kind {
+                AdjustKind::CoerceNever => expr,
+            };
+            expr.ty = adj.target;
+        }
+
+        expr
+    }
+}
+
+impl Default for Adjustments {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Adjust {
+    pub kind: AdjustKind,
+    pub target: Ty,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum AdjustKind {
+    CoerceNever,
 }
