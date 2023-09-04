8ba85b381c10ab35fcb523571f32a6aae57c5503
r0nsha <r0nsh4v1t@gmail.com>
2023-09-04 17:24:26 +0300

    wip

--- src/hir/pretty_print.rs
+++ src/hir/pretty_print.rs
@@ -2,14 +2,16 @@ use std::io;
 
 use crate::{
     db::Db,
-    hir::{Expr, ExprKind, Fn, Hir, Item, ItemKind, LitKind},
+    hir::{Expr, ExprKind, Fn, Hir, ItemKind, LitKind},
 };
 
 pub(super) fn print(db: &Db, hir: &Hir) -> io::Result<()> {
     let mut cx = PPCtxt { db, builder: ptree::TreeBuilder::new("Hir".to_string()) };
 
     for item in &hir.items {
-        item.pretty_print(&mut cx);
+        match &item.kind {
+            ItemKind::Fn(f) => cx.pp_fn(f),
+        }
     }
 
     let tree = cx.builder.build();
@@ -21,81 +23,105 @@ struct PPCtxt<'db> {
     builder: ptree::TreeBuilder,
 }
 
-trait PrettyPrint {
-    fn pretty_print(&self, cx: &mut PPCtxt);
-}
+impl PPCtxt<'_> {
+    fn pp_fn(&mut self, f: &Fn) {
+        self.builder.begin_child(format!(
+            "fn {} (returns: {})",
+            self.db[f.id].qpath,
+            f.ty.as_fn().expect("to be a function").ret.display(self.db)
+        ));
 
-impl PrettyPrint for Expr {
-    fn pretty_print(&self, cx: &mut PPCtxt) {
-        match &self.kind {
+        if !f.sig.params.is_empty() {
+            self.builder.begin_child("params".to_string());
+
+            for param in &f.sig.params {
+                self.builder.add_empty_child(format!(
+                    "{} (type: {})",
+                    self.db[param.id].name,
+                    param.ty.display(self.db)
+                ));
+            }
+
+            self.builder.end_child();
+        }
+
+        self.pp_expr(&f.body);
+
+        self.builder.end_child();
+    }
+
+    fn pp_expr(&mut self, expr: &Expr) {
+        match &expr.kind {
             ExprKind::If(if_) => {
-                cx.builder.begin_child("if".to_string());
+                self.builder.begin_child("if".to_string());
 
-                cx.builder.begin_child("cond".to_string());
-                if_.cond.pretty_print(cx);
-                cx.builder.end_child();
+                self.builder.begin_child("cond".to_string());
+                self.pp_expr(&if_.cond);
+                self.builder.end_child();
 
-                cx.builder.begin_child("then".to_string());
-                if_.then.pretty_print(cx);
-                cx.builder.end_child();
+                self.builder.begin_child("then".to_string());
+                self.pp_expr(&if_.then);
+                self.builder.end_child();
 
                 if let Some(otherwise) = &if_.otherwise {
-                    cx.builder.begin_child("else".to_string());
-                    otherwise.pretty_print(cx);
-                    cx.builder.end_child();
+                    self.builder.begin_child("else".to_string());
+                    self.pp_expr(otherwise);
+                    self.builder.end_child();
                 }
 
-                cx.builder.end_child();
+                self.builder.end_child();
             }
             ExprKind::Block(blk) => {
-                cx.builder.begin_child("block".to_string());
+                self.builder.begin_child("block".to_string());
 
                 for expr in &blk.exprs {
-                    expr.pretty_print(cx);
+                    self.pp_expr(expr);
                 }
 
-                cx.builder.end_child();
+                self.builder.end_child();
             }
             ExprKind::Return(ret) => {
-                cx.builder.begin_child("return".to_string());
-                ret.expr.pretty_print(cx);
-                cx.builder.end_child();
+                self.builder.begin_child("return".to_string());
+                self.pp_expr(&ret.expr);
+                self.builder.end_child();
             }
             ExprKind::Call(call) => {
-                cx.builder.begin_child(format!("call (result: {})", self.ty.display(cx.db)));
-                call.callee.pretty_print(cx);
+                self.builder.begin_child(format!("call (result: {})", expr.ty.display(self.db)));
+                self.pp_expr(&call.callee);
 
                 if !call.args.is_empty() {
-                    cx.builder.begin_child("args".to_string());
+                    self.builder.begin_child("args".to_string());
 
                     for arg in &call.args {
                         if let Some(name) = arg.name {
-                            cx.builder.begin_child(name.to_string());
-                            arg.expr.pretty_print(cx);
-                            cx.builder.end_child();
+                            self.builder.begin_child(name.to_string());
+                            self.pp_expr(&arg.expr);
+                            self.builder.end_child();
                         } else {
-                            arg.expr.pretty_print(cx);
+                            self.pp_expr(&arg.expr);
                         }
                     }
 
-                    cx.builder.end_child();
+                    self.builder.end_child();
                 }
 
-                cx.builder.end_child();
+                self.builder.end_child();
             }
             ExprKind::Bin(bin) => {
-                cx.builder.begin_child(format!("{} (result: {})", bin.op, self.ty.display(cx.db)));
-                bin.lhs.pretty_print(cx);
-                bin.rhs.pretty_print(cx);
-                cx.builder.end_child();
+                self.builder.begin_child(format!(
+                    "{} (result: {})",
+                    bin.op,
+                    expr.ty.display(self.db)
+                ));
+                self.pp_expr(&bin.lhs);
+                self.pp_expr(&bin.rhs);
+                self.builder.end_child();
             }
-            ExprKind::Name(inner) => {
-                let ref this = inner;
-                let cx: &mut PPCtxt = cx;
-                cx.builder.add_empty_child(format!(
+            ExprKind::Name(name) => {
+                self.builder.add_empty_child(format!(
                     "`{}` (type: {})",
-                    cx.db[this.id].qpath,
-                    self.ty.display(cx.db)
+                    self.db[name.id].qpath,
+                    expr.ty.display(self.db)
                 ));
             }
             ExprKind::Lit(lit) => {
@@ -105,45 +131,9 @@ impl PrettyPrint for Expr {
                     LitKind::Unit => "()".to_string(),
                 };
 
-                cx.builder
-                    .add_empty_child(format!("{value_str} (type: {})", self.ty.display(cx.db)));
+                self.builder
+                    .add_empty_child(format!("{value_str} (type: {})", expr.ty.display(self.db)));
             }
         }
-    }
-}
-
-impl PrettyPrint for Item {
-    fn pretty_print(&self, cx: &mut PPCtxt) {
-        match &self.kind {
-            ItemKind::Fn(fun) => fun.pretty_print(cx),
-        }
-    }
-}
-
-impl PrettyPrint for Fn {
-    fn pretty_print(&self, cx: &mut PPCtxt) {
-        cx.builder.begin_child(format!(
-            "fn {} (returns: {})",
-            cx.db[self.id].qpath,
-            self.ty.as_fn().expect("to be a function").ret.display(cx.db)
-        ));
-
-        if !self.sig.params.is_empty() {
-            cx.builder.begin_child("params".to_string());
-
-            for param in &self.sig.params {
-                cx.builder.add_empty_child(format!(
-                    "{} (type: {})",
-                    cx.db[param.id].name,
-                    param.ty.display(cx.db)
-                ));
-            }
-
-            cx.builder.end_child();
-        }
-
-        self.body.pretty_print(cx);
-
-        cx.builder.end_child();
     }
 }
