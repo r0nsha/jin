6c3e0166ffee914c4023ef2efe9fe823d8f01aa5
r0nsha <r0nsh4v1t@gmail.com>
2023-09-14 10:30:23 +0300

    wip

--- src/passes/resolve.rs
+++ src/passes/resolve.rs
@@ -1,6 +1,7 @@
-use std::collections::HashMap;
+mod env;
+mod error;
 
-use ustr::{ustr, Ustr, UstrMap};
+use ustr::{ustr, UstrMap};
 
 use crate::{
     ast::{
@@ -9,7 +10,10 @@ use crate::{
     },
     common::{QPath, Word},
     db::{Db, Def, DefId, DefKind, FnInfo, ModuleId, ModuleInfo, ScopeInfo, ScopeLevel, Vis},
-    diagnostics::{Diagnostic, Label},
+    passes::resolve::{
+        env::{Env, GlobalScope, ScopeKind},
+        error::ResolveError,
+    },
     span::{Span, Spanned},
     sym, ty,
 };
@@ -161,7 +165,7 @@ impl<'db> Resolver<'db> {
         let id = Def::alloc(
             self.db,
             env.scope_path(self.db).child(name.name()),
-            ScopeInfo { module_id: env.module_id, level: env.scope_level(), vis: Vis::Private },
+            ScopeInfo { module_id: env.module_id(), level: env.scope_level(), vis: Vis::Private },
             kind,
             self.db.types.unknown,
             name.span(),
@@ -185,7 +189,7 @@ impl<'db> Resolver<'db> {
         let name = word.name();
         env.lookup(name)
             .copied()
-            .or_else(|| self.global_scope.lookup(env.module_id, name))
+            .or_else(|| self.global_scope.lookup(env.module_id(), name))
             .or_else(|| self.builtins.get(&name).copied())
             .ok_or(ResolveError::NameNotFound(word))
     }
@@ -393,198 +397,6 @@ impl Resolve<'_> for TyName {
             self.id = Some(id);
         } else {
             cx.errors.push(ResolveError::NameNotFound(self.name));
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct GlobalScope {
-    modules: HashMap<(ModuleId, Ustr), DefId>,
-}
-
-impl GlobalScope {
-    pub fn new() -> Self {
-        Self { modules: HashMap::new() }
-    }
-
-    pub fn lookup(&self, module_id: ModuleId, name: Ustr) -> Option<DefId> {
-        self.modules.get(&(module_id, name)).copied()
-    }
-
-    pub fn insert(&mut self, module_id: ModuleId, name: Ustr, id: DefId) -> Option<DefId> {
-        self.modules.insert((module_id, name), id)
-    }
-}
-
-#[derive(Debug)]
-pub struct Env {
-    module_id: ModuleId,
-    scopes: Vec<Scope>,
-}
-
-#[allow(unused)]
-impl Env {
-    pub fn new(module_id: ModuleId) -> Self {
-        Self { module_id, scopes: vec![] }
-    }
-
-    pub fn push(&mut self, name: Ustr, kind: ScopeKind) {
-        self.scopes.push(Scope { kind, name, defs: UstrMap::default() });
-    }
-
-    pub fn pop(&mut self) -> Option<Scope> {
-        self.scopes.pop()
-    }
-
-    pub fn current(&self) -> &Scope {
-        self.scopes.last().expect("to have a scope")
-    }
-
-    pub fn current_mut(&mut self) -> &mut Scope {
-        self.scopes.last_mut().expect("to have a scope")
-    }
-
-    pub fn insert(&mut self, k: Ustr, v: DefId) {
-        self.scopes.last_mut().unwrap().defs.insert(k, v);
-    }
-
-    pub fn lookup(&self, k: Ustr) -> Option<&DefId> {
-        self.lookup_depth(k).map(|r| r.1)
-    }
-
-    #[allow(unused)]
-    pub fn lookup_mut(&mut self, k: Ustr) -> Option<&mut DefId> {
-        self.lookup_depth_mut(k).map(|r| r.1)
-    }
-
-    #[allow(unused)]
-    pub fn lookup_depth(&self, k: Ustr) -> Option<(usize, &DefId)> {
-        for (depth, scope) in self.scopes.iter().enumerate().rev() {
-            if let Some(value) = scope.defs.get(&k) {
-                return Some((depth + 1, value));
-            }
-        }
-        None
-    }
-
-    #[allow(unused)]
-    pub fn lookup_depth_mut(&mut self, k: Ustr) -> Option<(usize, &mut DefId)> {
-        for (depth, scope) in self.scopes.iter_mut().enumerate().rev() {
-            if let Some(value) = scope.defs.get_mut(&k) {
-                return Some((depth + 1, value));
-            }
-        }
-        None
-    }
-
-    pub fn scope_level(&self) -> ScopeLevel {
-        match self.depth() {
-            0 => unreachable!("expected to have at least one scope"),
-            n => ScopeLevel::Local(n),
-        }
-    }
-
-    pub fn scope_path(&self, db: &Db) -> QPath {
-        let mut qpath = db[self.module_id].name.clone();
-        qpath.extend(self.scopes.iter().map(|s| s.name));
-        qpath
-    }
-
-    #[inline]
-    pub fn depth(&self) -> usize {
-        self.scopes.len()
-    }
-
-    #[inline]
-    pub fn in_global_scope(&self) -> bool {
-        self.depth() == 0
-    }
-
-    fn in_kind(&self, kind: ScopeKind) -> bool {
-        self.scopes.iter().any(|s| s.kind == kind)
-    }
-}
-
-#[derive(Debug)]
-pub struct Scope {
-    pub kind: ScopeKind,
-    pub name: Ustr,
-    pub defs: UstrMap<DefId>,
-}
-
-impl Scope {
-    fn insert(&mut self, name: Ustr, id: DefId) {
-        self.defs.insert(name, id);
-    }
-
-    #[allow(unused)]
-    fn get(&mut self, name: Ustr) -> Option<DefId> {
-        self.defs.get(&name).copied()
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub enum ScopeKind {
-    Fn,
-    Block,
-}
-
-pub(super) enum ResolveError {
-    MultipleItems { name: Ustr, prev_span: Span, dup_span: Span },
-    MultipleParams { name: Ustr, prev_span: Span, dup_span: Span },
-    MultipleTyParams { name: Ustr, prev_span: Span, dup_span: Span },
-    NameNotFound(Word),
-    InvalidReturn(Span),
-    InvalidInferTy(Span),
-}
-
-impl From<ResolveError> for Diagnostic {
-    fn from(err: ResolveError) -> Self {
-        match err {
-            ResolveError::MultipleItems { name, prev_span, dup_span } => {
-                Self::error("resolve::multiple_items")
-                    .with_message(format!("the item `{name}` is defined multiple times"))
-                    .with_label(
-                        Label::primary(dup_span)
-                            .with_message(format!("`{name}` defined again here")),
-                    )
-                    .with_label(
-                        Label::secondary(prev_span)
-                            .with_message(format!("first definition of `{name}`")),
-                    )
-                    .with_help("you can only define items once in a module")
-            }
-            ResolveError::MultipleTyParams { name, prev_span, dup_span } => {
-                Self::error("resolve::multiple_type_params")
-                    .with_message(format!(
-                        "the name `{name}` is already used as a type parameter name"
-                    ))
-                    .with_label(
-                        Label::primary(dup_span).with_message(format!("`{name}` used again here")),
-                    )
-                    .with_label(
-                        Label::secondary(prev_span).with_message(format!("first use of `{name}`")),
-                    )
-            }
-            ResolveError::MultipleParams { name, prev_span, dup_span } => {
-                Self::error("resolve::multiple_params")
-                    .with_message(format!("the name `{name}` is already used as a parameter name"))
-                    .with_label(
-                        Label::primary(dup_span).with_message(format!("`{name}` used again here")),
-                    )
-                    .with_label(
-                        Label::secondary(prev_span).with_message(format!("first use of `{name}`")),
-                    )
-            }
-            ResolveError::NameNotFound(name) => Self::error("resolve::name_not_found")
-                .with_message(format!("cannot find `{name}` in this scope"))
-                .with_label(Label::primary(name.span()).with_message("not found in this scope")),
-            ResolveError::InvalidReturn(span) => Self::error("resolve::invalid_return")
-                .with_message("cannot return outside of function scope")
-                .with_label(Label::primary(span)),
-            ResolveError::InvalidInferTy(span) => Self::error("resolve::invalid_infer_type")
-                .with_message("cannot use a _ type in a function's signature")
-                .with_label(Label::primary(span)),
         }
     }
 }
---
+++ src/passes/resolve/env.rs
@@ -0,0 +1,144 @@
+use std::collections::HashMap;
+
+use ustr::{Ustr, UstrMap};
+
+use crate::{
+    common::QPath,
+    db::{Db, DefId, ModuleId, ScopeLevel},
+};
+
+#[derive(Debug)]
+pub struct GlobalScope {
+    modules: HashMap<(ModuleId, Ustr), DefId>,
+}
+
+impl GlobalScope {
+    pub fn new() -> Self {
+        Self { modules: HashMap::new() }
+    }
+
+    pub fn lookup(&self, module_id: ModuleId, name: Ustr) -> Option<DefId> {
+        self.modules.get(&(module_id, name)).copied()
+    }
+
+    pub fn insert(&mut self, module_id: ModuleId, name: Ustr, id: DefId) -> Option<DefId> {
+        self.modules.insert((module_id, name), id)
+    }
+}
+
+#[derive(Debug)]
+pub struct Env {
+    module_id: ModuleId,
+    scopes: Vec<Scope>,
+}
+
+#[allow(unused)]
+impl Env {
+    pub fn new(module_id: ModuleId) -> Self {
+        Self { module_id, scopes: vec![] }
+    }
+
+    pub fn push(&mut self, name: Ustr, kind: ScopeKind) {
+        self.scopes.push(Scope { kind, name, defs: UstrMap::default() });
+    }
+
+    pub fn pop(&mut self) -> Option<Scope> {
+        self.scopes.pop()
+    }
+
+    pub fn current(&self) -> &Scope {
+        self.scopes.last().expect("to have a scope")
+    }
+
+    pub fn current_mut(&mut self) -> &mut Scope {
+        self.scopes.last_mut().expect("to have a scope")
+    }
+
+    pub fn insert(&mut self, k: Ustr, v: DefId) {
+        self.scopes.last_mut().unwrap().defs.insert(k, v);
+    }
+
+    pub fn lookup(&self, k: Ustr) -> Option<&DefId> {
+        self.lookup_depth(k).map(|r| r.1)
+    }
+
+    #[allow(unused)]
+    pub fn lookup_mut(&mut self, k: Ustr) -> Option<&mut DefId> {
+        self.lookup_depth_mut(k).map(|r| r.1)
+    }
+
+    #[allow(unused)]
+    pub fn lookup_depth(&self, k: Ustr) -> Option<(usize, &DefId)> {
+        for (depth, scope) in self.scopes.iter().enumerate().rev() {
+            if let Some(value) = scope.defs.get(&k) {
+                return Some((depth + 1, value));
+            }
+        }
+        None
+    }
+
+    #[allow(unused)]
+    pub fn lookup_depth_mut(&mut self, k: Ustr) -> Option<(usize, &mut DefId)> {
+        for (depth, scope) in self.scopes.iter_mut().enumerate().rev() {
+            if let Some(value) = scope.defs.get_mut(&k) {
+                return Some((depth + 1, value));
+            }
+        }
+        None
+    }
+
+    pub fn scope_level(&self) -> ScopeLevel {
+        match self.depth() {
+            0 => unreachable!("expected to have at least one scope"),
+            n => ScopeLevel::Local(n),
+        }
+    }
+
+    pub fn scope_path(&self, db: &Db) -> QPath {
+        let mut qpath = db[self.module_id].name.clone();
+        qpath.extend(self.scopes.iter().map(|s| s.name));
+        qpath
+    }
+
+    #[inline]
+    pub fn depth(&self) -> usize {
+        self.scopes.len()
+    }
+
+    #[inline]
+    pub fn in_global_scope(&self) -> bool {
+        self.depth() == 0
+    }
+
+    pub fn in_kind(&self, kind: ScopeKind) -> bool {
+        self.scopes.iter().any(|s| s.kind == kind)
+    }
+
+    pub fn module_id(&self) -> ModuleId {
+        self.module_id
+    }
+}
+
+#[derive(Debug)]
+pub struct Scope {
+    pub kind: ScopeKind,
+    pub name: Ustr,
+    pub defs: UstrMap<DefId>,
+}
+
+impl Scope {
+    #[allow(unused)]
+    pub fn get(&mut self, name: Ustr) -> Option<DefId> {
+        self.defs.get(&name).copied()
+    }
+
+    pub fn insert(&mut self, name: Ustr, id: DefId) {
+        self.defs.insert(name, id);
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum ScopeKind {
+    Fn,
+    Block,
+}
