78c2cdc0ccd2a3dde8e988128196b2ea3ec52987
r0nsha <r0nsh4v1t@gmail.com>
2023-09-20 17:13:55 +0300

    str_ty

--- src/llvm/layout.rs
+++ src/llvm/layout.rs
@@ -2,11 +2,15 @@ use inkwell::{
     context::Context,
     targets::TargetMachine,
     types::{IntType, StructType},
+    AddressSpace,
 };
+
+use crate::llvm::inkwell_ext::ContextExt;
 
 pub struct Layout<'cx> {
     pub int_ty: IntType<'cx>,
     pub unit_ty: StructType<'cx>,
+    pub str_ty: StructType<'cx>,
 }
 
 impl<'cx> Layout<'cx> {
@@ -14,9 +18,11 @@ impl<'cx> Layout<'cx> {
         let unit_ty = context.opaque_struct_type("unit");
         unit_ty.set_body(&[], false);
 
-        Self {
-            int_ty: context.ptr_sized_int_type(&target_machine.get_target_data(), None),
-            unit_ty,
-        }
+        let int_ty = context.ptr_sized_int_type(&target_machine.get_target_data(), None);
+
+        let str_ty = context.opaque_struct_type("str");
+        str_ty.set_body(&[context.ptr_type(AddressSpace::default()).into(), int_ty.into()], false);
+
+        Self { int_ty, unit_ty, str_ty }
     }
 }
