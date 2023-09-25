c8bb457e0dccf839fa5ff3e52f7194cca9f524e3
r0nsha <r0nsh4v1t@gmail.com>
2023-09-25 23:37:47 +0300

    use = for named args

--- src/parse/parser.rs
+++ src/parse/parser.rs
@@ -517,7 +517,7 @@ impl<'a> Parser<'a> {
         if self.is(TokenKind::empty_ident()) {
             let ident_tok = self.last_token();
 
-            if self.is(TokenKind::Colon) {
+            if self.is(TokenKind::Eq) {
                 let expr = self.parse_expr()?;
                 return Ok(CallArg::Named(ident_tok.word(), expr));
             }
