87fefec6688d1843b37f2750be9dd420792ca5f4
r0nsha <r0nsh4v1t@gmail.com>
2023-10-15 00:46:03 +0300

    wip

--- src/parse/parser.rs
+++ src/parse/parser.rs
@@ -255,14 +255,14 @@ impl<'a> Parser<'a> {
             };
 
             let op = match BinOp::try_from(tok.kind).ok() {
-                // For these specific operators, we check if they are on the same line as the last
-                // expr, to avoid ambiguity with unary operators
                 Some(BinOp::BitAnd | BinOp::Sub)
                     if !self.spans_are_on_same_line(
                         expr_stack.last().expect("to have an expr").span(),
                         tok.span,
                     ) =>
                 {
+                    // For these specific operators, we check if they are on the same line as the last
+                    // expr, to avoid ambiguity with unary operators
                     break;
                 }
                 None => break,
