8c29ae60be7d9d4f78d2c04bdeeed36a400dc348
r0nsha <r0nsh4v1t@gmail.com>
2023-12-14 12:06:06 +0200

    wip

--- src/ownck.rs
+++ src/ownck.rs
@@ -1,13 +1,14 @@
 use crate::{
     db::Db,
-    hir::{self, Hir},
+    hir::{self, Hir, HirMap},
+    span::Span,
 };
 
 pub fn ownck(db: &Db, hir: &mut Hir) {
     for fun in &hir.fns {
         match &fun.kind {
             hir::FnKind::Bare { body } => {
-                Ownck::new(db).expr(body);
+                Ownck::new(db).expr(&mut Env::new(), body);
                 todo!("collect into destroy_glue")
             }
             hir::FnKind::Extern { .. } => (),
@@ -21,50 +22,84 @@ pub fn ownck(db: &Db, hir: &mut Hir) {
 
 struct Ownck<'db> {
     db: &'db Db,
-    scopes: Scopes,
 }
 
 impl<'db> Ownck<'db> {
     fn new(db: &'db Db) -> Self {
-        Self { db, scopes: Scopes::new() }
+        Self { db }
     }
 
-    fn expr(&mut self, expr: &hir::Expr) {
-        todo!("expr")
+    fn expr(&mut self, env: &mut Env, expr: &hir::Expr) {
+        match &expr.kind {
+            hir::ExprKind::Let(_) => todo!(),
+            hir::ExprKind::Assign(_) => todo!(),
+            hir::ExprKind::If(_) => todo!(),
+            hir::ExprKind::Loop(_) => todo!(),
+            hir::ExprKind::Break => todo!(),
+            hir::ExprKind::Block(block) => env.with(|env| {
+                block.exprs.iter().for_each(|expr| self.expr(env, expr));
+            }),
+            hir::ExprKind::Return(_) => todo!(),
+            hir::ExprKind::Call(call) => {
+                self.expr(env, &call.callee);
+                call.args.iter().for_each(|arg| self.expr(env, &arg.expr));
+            }
+            hir::ExprKind::Member(_) => todo!(),
+            hir::ExprKind::Name(_) => (),
+            hir::ExprKind::Unary(_)
+            | hir::ExprKind::Binary(_)
+            | hir::ExprKind::Cast(_)
+            | hir::ExprKind::Lit(_) => {
+                env.create_owned(expr);
+            }
+        }
     }
 }
 
 #[derive(Debug)]
-pub struct Scopes(Vec<Scope>);
+struct Env(Vec<Scope>);
 
-impl Scopes {
-    pub fn new() -> Self {
+impl Env {
+    fn new() -> Self {
         Self(vec![])
     }
 
-    pub fn with_scope<R>(&mut self, mut f: impl FnMut(&mut Self) -> R) -> R {
-        self.0.push(Scope { value_states: vec![] });
+    fn with<R>(&mut self, mut f: impl FnMut(&mut Self) -> R) -> R {
+        self.0.push(Scope { expr_states: HirMap::default() });
         let res = f(self);
         self.0.pop();
         res
     }
 
-    pub fn create_owned(&mut self) {
-        todo!("create owned value state")
+    fn create_owned(&mut self, expr: &hir::Expr) {
+        self.current_mut().expr_states.insert(
+            expr.id,
+            Value { state: ValueState::Owned, span: expr.span },
+        );
     }
 
-    pub fn lookup(&self) -> Option<ValueState> {
+    fn lookup(&self) -> Option<Value> {
         todo!("iter in reverse")
     }
+
+    fn current_mut(&mut self) -> &mut Scope {
+        self.0.last_mut().unwrap()
+    }
 }
 
 #[derive(Debug)]
-pub struct Scope {
-    pub value_states: Vec<ValueState>,
+struct Scope {
+    expr_states: HirMap<Value>,
 }
 
 #[derive(Debug)]
-pub enum ValueState {
+struct Value {
+    state: ValueState,
+    span: Span,
+}
+
+#[derive(Debug)]
+enum ValueState {
     Owned,
     Moved,
 }
