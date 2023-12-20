d0759d2830f24736d37a402eb88bbaa768d866e0
r0nsha <r0nsh4v1t@gmail.com>
2023-12-20 15:41:14 +0200

    IdMap implementation

--- src/id_map.rs
+++ src/id_map.rs
@@ -9,6 +9,28 @@ pub struct IdMap<K, V> {
     counter: Counter<K>,
 }
 
+impl<K, V> IdMap<K, V> {
+    pub fn iter(&self) -> impl Iterator<Item = (&K, &V)> {
+        self.map.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = (&K, &mut V)> {
+        self.map.iter_mut()
+    }
+
+    pub fn keys(&self) -> impl Iterator<Item = &K> {
+        self.map.keys()
+    }
+
+    pub fn values(&self) -> impl Iterator<Item = &V> {
+        self.map.values()
+    }
+
+    pub fn values_mut(&mut self) -> impl Iterator<Item = &mut V> {
+        self.map.values_mut()
+    }
+}
+
 impl<K: Eq + Hash, V> IdMap<K, V> {
     pub fn get(&self, key: &K) -> Option<&V> {
         self.map.get(key)
