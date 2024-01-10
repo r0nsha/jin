b7690abdaeba39744c458ae4bfd049d72e68532e
r0nsha <r0nsh4v1t@gmail.com>
2024-01-10 16:14:11 +0200

    fix run.fish

--- run.fish
+++ run.fish
@@ -3,12 +3,12 @@ if set -q $argv[1]
     echo Usage: run.fish [FILE_NAME]
 else
     set -l source $argv[1]
-    set -l source_dir (dirname source)
-    set -l output_name (string split . (string split / tests/swap.jin)[-1])[1]
+    set -l source_dir (dirname $source)
+    set -l output_name (basename $source .jin)
     set -l output $source_dir/build/$output_name
 
     cargo run -- build $source --timings --emit ast --emit hir --emit mir --emit c
-    
+
     if test $status -eq 0 && test -f $output
         cp -r std target/std
         $output
