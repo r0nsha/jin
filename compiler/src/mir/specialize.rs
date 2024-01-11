8ca0847e26b72d61cd720a679b6f536c97ab35d0
r0nsha <r0nsh4v1t@gmail.com>
2024-01-11 12:13:36 +0200

    wip

--- compiler/src/mir/specialize.rs
+++ compiler/src/mir/specialize.rs
@@ -4,7 +4,7 @@ use rustc_hash::FxHashSet;
 use ustr::ustr;
 
 use crate::{
-    db::Db,
+    db::{AdtId, Db},
     mangle,
     mir::{
         Body, Fn, FnSig, FnSigId, FxHashMap, GlobalId, GlobalKind, IdMap, Inst,
@@ -28,6 +28,9 @@ struct Specialize<'db> {
 
     // Functions that have already been specialized and should be re-used
     specialized_fns: FxHashMap<SpecializedFn, FnSigId>,
+
+    // Generated free functions for adt types
+    adt_frees: FxHashMap<(AdtId, Vec<Ty>), FnSigId>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -44,6 +47,7 @@ impl<'db> Specialize<'db> {
             used_fns: FxHashSet::default(),
             used_globals: FxHashSet::default(),
             specialized_fns: FxHashMap::default(),
+            adt_frees: FxHashMap::default(),
         }
     }
 
@@ -399,3 +403,157 @@ impl TyFolder for SpecializeParamFolder<'_> {
         }
     }
 }
+
+// fn get_or_create_free_fn(&mut self, ty: Ty) -> Option<FnSigId> {
+//     match ty.kind() {
+//         TyKind::Adt(adt_id, _) => match self.db[*adt_id].kind {
+//             AdtKind::Struct(_) => None,
+//             AdtKind::Union(_) => Some(self.get_or_create_adt_free(*adt_id)),
+//         },
+//         _ => None,
+//     }
+// }
+
+// fn get_or_create_adt_free(&mut self, adt_id: AdtId) -> FnSigId {
+//     if let Some(sig_id) = self.adt_frees.get(&adt_id) {
+//         return *sig_id;
+//     }
+//
+//     let sig_id = CreateAdtFree::new(self, adt_id).create();
+//     self.adt_frees.insert(adt_id, sig_id);
+//
+//     sig_id
+// }
+
+// #[derive(Debug)]
+// pub(super) struct CreateAdtFree<'cx, 'db> {
+//     pub(super) cx: &'cx mut Lower<'db>,
+//     pub(super) adt_id: AdtId,
+//     pub(super) body: Body,
+//     pub(super) current_block: BlockId,
+// }
+//
+// impl<'cx, 'db> CreateAdtFree<'cx, 'db> {
+//     pub(super) fn new(cx: &'cx mut Lower<'db>, adt_id: AdtId) -> Self {
+//         Self { cx, adt_id, body: Body::new(), current_block: BlockId::start() }
+//     }
+//
+//     fn create(mut self) -> FnSigId {
+//         let adt = &self.cx.db[self.adt_id];
+//         let adt_ty = adt.ty();
+//
+//         let name = ustr(
+//             &self.cx.db[adt.def_id]
+//                 .qpath
+//                 .clone()
+//                 .child(ustr("free"))
+//                 .join_with("_"),
+//         );
+//         let self_name = ustr("self");
+//
+//         let params = vec![FnParam {
+//             pat: Pat::Name(NamePat {
+//                 id: DefId::null(),
+//                 word: Word::new(self_name, Span::unknown()),
+//                 vis: Vis::Private,
+//                 mutability: Mutability::Imm,
+//                 ty: adt_ty,
+//             }),
+//             ty: adt_ty,
+//         }];
+//
+//         let fn_ty = Ty::new(TyKind::Fn(FnTy {
+//             params: params
+//                 .iter()
+//                 .map(|p| FnTyParam {
+//                     name: Some(p.pat.name().unwrap()),
+//                     ty: p.ty,
+//                 })
+//                 .collect(),
+//             ret: self.cx.db.types.unit,
+//             is_c_variadic: false,
+//         }));
+//
+//         let sig = self.cx.mir.fn_sigs.insert_with_key(|id| FnSig {
+//             id,
+//             name,
+//             params,
+//             ty: fn_ty,
+//             is_extern: false,
+//             is_c_variadic: false,
+//             span: adt.name.span(),
+//         });
+//
+//         let start_block = self.body.create_block("start");
+//         self.current_block = start_block;
+//
+//         let self_value =
+//             self.body.create_value(adt_ty, ValueKind::UniqueName(self_name));
+//
+//         match &adt.kind {
+//             AdtKind::Union(union_def) => {
+//                 let mut blocks = vec![];
+//
+//                 for &variant_id in &union_def.variants {
+//                     let variant = &self.cx.db[variant_id];
+//                     let variant_value = self.body.create_value(
+//                         adt_ty,
+//                         ValueKind::Variant(self_value, variant.name.name()),
+//                     );
+//                     let block =
+//                         self.lower_variant_free(&variant, variant_value);
+//                     blocks.push(block);
+//                 }
+//
+//                 self.current_block = start_block;
+//                 let uint = self.cx.db.types.uint;
+//                 let tag_field = self.body.create_value(
+//                     uint,
+//                     ValueKind::Field(self_value, ustr("tag")),
+//                 );
+//                 self.body.switch(start_block, tag_field, blocks);
+//             }
+//             AdtKind::Struct(struct_def) => {
+//                 unreachable!()
+//             }
+//         }
+//
+//         self.cx.mir.fns.insert(sig, Fn { sig, body: self.body });
+//
+//         sig
+//     }
+//
+//     fn lower_variant_free(
+//         &mut self,
+//         variant: &Variant,
+//         variant_value: ValueId,
+//     ) -> BlockId {
+//         let block = self.body.create_block(format!("case_{}", variant.name));
+//         self.current_block = block;
+//
+//         for field in &variant.fields {
+//             let value = self.body.create_value(
+//                 field.ty,
+//                 ValueKind::Field(variant_value, field.name.name()),
+//             );
+//
+//             match field.ty.kind() {
+//                 TyKind::Adt(adt_id, _) => match &self.cx.db[*adt_id].kind {
+//                     AdtKind::Struct(struct_def) => todo!(),
+//                     AdtKind::Union(union_def) => todo!(),
+//                 },
+//                 TyKind::Ref(..) | TyKind::Param(_) => {
+//                     // TODO: location param
+//                     self.push_inst(Inst::Free { value, span: field.span() });
+//                 }
+//                 _ => (),
+//             }
+//         }
+//
+//         block
+//     }
+//
+//     pub fn push_inst(&mut self, inst: Inst) {
+//         self.body.block_mut(self.current_block).push_inst(inst);
+//     }
+// }
