fd6c2ba33c054e24217d04f212279a928fec7674
r0nsha <r0nsh4v1t@gmail.com>
2024-01-13 21:19:03 +0200

    wip

--- rt/jinrt.h
+++ rt/jinrt.h
@@ -1,4 +1,5 @@
-#include <math.h>
+#ifndef JINRT_H
+#define JINRT_H
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -48,40 +49,4 @@ void jin_rt_panic(u8 *msg);
 void jin_rt_panic_at(u8 *msg, jin_rt_location loc);
 void jin_rt_refcheck(usize refcnt, u8 *fmt, jin_rt_location loc);
 bool jin_rt_strcmp(str a, str b);
-
-FORCE_INLINE void *jin_rt_alloc(size_t size) {
-  void *ptr = malloc(size);
-
-  if (!ptr) {
-    jin_rt_panic("out of memory");
-  }
-
-  return ptr;
-}
-
-FORCE_INLINE void jin_rt_free(void *ptr) { free(ptr); }
-
-FORCE_INLINE void jin_rt_panic(u8 *msg) {
-  dprintf(STDERR, "panic at '%s'\n", msg);
-  exit(1);
-}
-
-FORCE_INLINE void jin_rt_panic_at(u8 *msg, jin_rt_location loc) {
-  dprintf(STDERR, "panic at '%s', %s:%d:%d\n", msg, loc.path, loc.line,
-          loc.column);
-  exit(1);
-}
-
-void jin_rt_refcheck(usize refcnt, u8 *fmt, jin_rt_location loc) {
-  if (refcnt != 0) {
-    size_t needed = snprintf(NULL, 0, fmt, refcnt);
-    char *msg = malloc(needed);
-    sprintf(msg, fmt, refcnt);
-    jin_rt_panic_at(msg, loc);
-  }
-}
-
-FORCE_INLINE bool jin_rt_strcmp(str a, str b) {
-  return strcmp(a.ptr, b.ptr) == 0;
-}
-
+#endif
