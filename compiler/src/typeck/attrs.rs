12bc353695e7d46c276c9b865535a6bcf1d7ba7b
r0nsha <r0nsh4v1t@gmail.com>
2024-01-19 15:07:12 +0200

    validate attr placement

--- compiler/src/typeck/attrs.rs
+++ compiler/src/typeck/attrs.rs
@@ -3,29 +3,39 @@ use crate::{
     typeck::{errors::invalid_attr_placement, Typeck, TypeckResult},
 };
 
-impl<'db> Typeck<'db> {
-    pub fn check_attrs(
-        &mut self,
-        attrs: &ast::Attrs,
-        placement: AttrsPlacement,
-    ) -> TypeckResult<()> {
-        for attr in attrs {
-            match attr.kind {
-                ast::AttrKind::Intrinsic => match placement {
-                    AttrsPlacement::ExternFn => todo!(),
-                    _ => return Err(invalid_attr_placement(attr, "fn extern")),
-                },
-            }
-        }
-
-        Ok(())
-    }
-}
-
 #[derive(Debug, Clone, Copy)]
 pub enum AttrsPlacement {
     Fn,
     ExternFn,
     Let,
     ExternLet,
+}
+
+impl<'db> Typeck<'db> {
+    #[allow(clippy::unused_self)]
+    pub fn check_attrs(
+        &self,
+        attrs: &ast::Attrs,
+        placement: AttrsPlacement,
+    ) -> TypeckResult<()> {
+        for attr in attrs {
+            validate_attr_placement(attr, placement).map_err(|applies_to| {
+                invalid_attr_placement(attr, applies_to)
+            })?;
+        }
+
+        Ok(())
+    }
+}
+
+fn validate_attr_placement(
+    attr: &ast::Attr,
+    placement: AttrsPlacement,
+) -> Result<(), &'static str> {
+    match attr.kind {
+        ast::AttrKind::Intrinsic => match placement {
+            AttrsPlacement::ExternFn => Ok(()),
+            _ => Err("fn extern"),
+        },
+    }
 }
