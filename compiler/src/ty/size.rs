579575e1b9598558f6b1415d21ccba905cdb4d12
r0nsha <r0nsh4v1t@gmail.com>
2024-01-29 11:56:45 +0200

    lints

--- compiler/src/ty/size.rs
+++ compiler/src/ty/size.rs
@@ -141,10 +141,12 @@ impl FloatTy {
     }
 }
 
+#[allow(unused)]
 fn bit_width_to_size(w: u32) -> usize {
     ((w + 7) / 8) as usize
 }
 
+#[allow(unused)]
 fn calculate_align(size: usize, align: usize) -> usize {
     if align > 0 {
         let res = size + align - 1;
