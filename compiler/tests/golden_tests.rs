8c0f1e6952f52cab6b1fc22e1b761c13d8b8ad7a
r0nsha <r0nsh4v1t@gmail.com>
2024-02-21 12:30:05 +0200

    wip

--- compiler/tests/golden_tests.rs
+++ compiler/tests/golden_tests.rs
@@ -2,6 +2,6 @@ use goldentests::{TestConfig, TestResult};
 
 #[test]
 fn goldentests() -> TestResult<()> {
-    let config = TestConfig::new("../target/debug/jin", "tests", "# ")?;
+    let config = TestConfig::new("../target/debug/jin", "goldentests", "# ")?;
     config.run_tests()
 }
