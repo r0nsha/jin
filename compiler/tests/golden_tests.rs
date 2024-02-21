50eb454502db7d115b63b7ee1709887bfc4bae0c
r0nsha <r0nsh4v1t@gmail.com>
2024-02-21 12:34:37 +0200

    remove goldentests

--- compiler/tests/golden_tests.rs
+++
@@ -1,7 +0,0 @@
-use goldentests::{TestConfig, TestResult};
-
-#[test]
-fn goldentests() -> TestResult<()> {
-    let config = TestConfig::new("../target/debug/jin", "goldentests", "# ")?;
-    config.run_tests()
-}
