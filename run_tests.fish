63a2a24e19fd48f7d54e7dd4517aeecd9073a29a
r0nsha <r0nsh4v1t@gmail.com>
2024-02-21 14:57:32 +0200

    nl

--- run_tests.fish
+++ run_tests.fish
@@ -4,6 +4,7 @@ cargo build
 time for file in (fd . tests -e jin -d 1)
     echo (set_color -o brgreen)Running (set_color normal)$file
     target/debug/jin run $file -o build
+    echo
 end
 
 rm -r tests/build
