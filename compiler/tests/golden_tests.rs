ce8677126fb2bcfe6d834e529aefc08aa20f3193
r0nsha <r0nsh4v1t@gmail.com>
2024-02-21 12:24:33 +0200

    fix path

--- compiler/tests/golden_tests.rs
+++ compiler/tests/golden_tests.rs
@@ -2,6 +2,6 @@ use goldentests::{TestConfig, TestResult};
 
 #[test]
 fn goldentests() -> TestResult<()> {
-    let config = TestConfig::new("target/debug/jin", "tests", "# ")?;
+    let config = TestConfig::new("../target/debug/jin", "tests", "# ")?;
     config.run_tests()
 }
