023ce474c13639c003f122b3087d13264270957b
r0nsha <r0nsh4v1t@gmail.com>
2024-03-17 15:21:59 +0200

    run tests/examples in release mode

--- run_tests.fish
+++ run_tests.fish
@@ -1,9 +1,9 @@
 #!/bin/fish
-cargo build
+cargo build -r
 
 time for file in (fd . tests -e jin -d 1)
     echo (set_color -o brgreen)Running (set_color normal)$file
-    target/debug/jin run $file -o build
+    target/release/jin run $file -o build
     echo
 end
 
