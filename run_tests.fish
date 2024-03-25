4334f058bbf673844147dceb7c9ea7c35476cc8f
r0nsha <r0nsh4v1t@gmail.com>
2024-03-25 17:25:24 +0200

    update tests dir

--- run_tests.fish
+++ run_tests.fish
@@ -1,9 +1,11 @@
 #!/bin/fish
 cargo build -r
 
-time for file in (fd . tests -e jin -d 1)
+time for dir in (fd . tests -d 1)
+    set -l file $dir/main.jin
     echo (set_color -o brgreen)Running (set_color normal)$file
-    target/release/jin run $file -o build
+    target/release/jin run $file -o test-out
+    rm -r $dir/test-out
     echo
 end
 
